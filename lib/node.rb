e52dbf3201400e2c3b5f646ce37b7aae2857e3e5
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-24 22:08:29 -0700

    Add initial requirements to node_spec.rb  and inital structure of Node class and Linked List clast to node.rb" "

---
+++ lib/node.rb
@@ -0,0 +1,10 @@
+class Node
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+end
+
+class Linked List
+end
---
+++ spec/node_spec.rb
@@ -0,0 +1,8 @@
+require './lib/node'
+require 'pry'
+require 'rspec'
+
+describe 'node' do
+  it 'has a node' do
+
+node = Node.new("plop")
