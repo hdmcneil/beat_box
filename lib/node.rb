5c28abac8825993688cfc550d554b8671982077d
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-24 22:43:20 -0700

    Add next_node method to node.rb and add tests for new instance of node, node.data, and node.next_node

--- lib/node.rb
+++ lib/node.rb
@@ -1,10 +1,22 @@
 class Node
-  attr_reader :data
+  attr_accessor :data, :next
 
   def initialize(data)
     @data = data
+    @next = nil
+  end
+
+  def next_node
+    @next
   end
 end
 
-class Linked List
+class LinkedList
+  attr_accessor :head
+
+  def initialize
+  
+  end
+
+
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,5 +4,22 @@ require 'rspec'
 
 describe 'node' do
   it 'has a node' do
+  node = Node.new("plop")
+  expect(node).to be_instance_of(Node)
+  end
 
-node = Node.new("plop")
+  it 'has a value' do
+    node = Node.new("plop")
+    node.data
+    expect(node.data).to eq("plop")
+  end
+
+  it 'does not have another node' do
+    node = Node.new("plop")
+    node.next_node
+    expect(node.next_node).to eq(nil)
+  end
+
+
+
+end
