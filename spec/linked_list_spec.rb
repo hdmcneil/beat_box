2cdc4c9332b936bab4d29ac7e486296891d04600
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-26 13:40:51 -0700

    Add to_string testing

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -5,30 +5,36 @@ require 'pry'
 RSpec.describe LinkedList do
   before :each do
     @list = LinkedList.new
+  end
+
   describe '#initialize' do
   it 'has a new Linked List' do
-  expect(list).to be_instance_of(LinkedList)
+  expect(@list).to be_instance_of(LinkedList)
   end
 
   it 'has a head' do
-    list.head
-    expect(list.head).to eq(nil)
+    @list.head
+    expect(@list.head).to eq(nil)
   end
 
   it 'has data value for 1st node' do
-    list.append("doop")
-    list
-    expect(list).to be_instance_of(LinkedList)
+    @list.append("doop")
+    @list
+    expect(@list).to be_instance_of(LinkedList)
   end
 
-  list.head.data
-  list.head.next_node
+  
 
   it 'counts the nodes' do
-    list.append("doop")
-    expext(list.count).to eq(1)
+    @list.append("doop")
+    expect(@list.count).to eq(1)
   end
 
-end
+  it 'creates a string of all the nodes' do
+    @list.to_string
+    @list.append("doop")
+    expect(@list.to_string).to eq("doop")
+  end
+
 end
 end
