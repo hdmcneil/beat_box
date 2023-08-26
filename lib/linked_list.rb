e8f0ac654f68e7104be2c58eb1681c9c67b19ca0
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-25 22:08:22 -0700

    Add count method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,27 @@ class LinkedList
   def head
     @head
   end
+
+  def append(value)
+    if 
+      @head.nil?
+      @head = Node.new(value)
+    else
+      last_node = @head
+      while(last_node.next_node.nil? != true)
+      end
+    end
+  end
+
+  def count
+    count = 0
+    last_node = @head
+    until last_node.nil?
+      count += 1
+      last_node = last_node.next_node
+    end
+    count
+  end
   
 
 end
