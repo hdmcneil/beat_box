5587cba81c42f2a7c7abb50c8302f2a39ed17a2d
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-25 22:10:24 -0700

    Add append method and count method testing

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -3,19 +3,32 @@ require './lib/node'
 require 'pry'
 
 RSpec.describe LinkedList do
+  before :each do
+    @list = LinkedList.new
   describe '#initialize' do
   it 'has a new Linked List' do
-  list = LinkedList.new
   expect(list).to be_instance_of(LinkedList)
   end
 
   it 'has a head' do
-    list = LinkedList.new
     list.head
     expect(list.head).to eq(nil)
   end
 
-  
+  it 'has data value for 1st node' do
+    list.append("doop")
+    list
+    expect(list).to be_instance_of(LinkedList)
+  end
 
+  list.head.data
+  list.head.next_node
+
+  it 'counts the nodes' do
+    list.append("doop")
+    expext(list.count).to eq(1)
+  end
+
+end
 end
 end
