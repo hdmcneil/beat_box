f1719fea923700cc3fce6ba9331a28eb03a58bdf
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-27 16:04:34 -0700

    Add beat_box_spec.rb and add requirements

---
+++ spec/beat_box_spec.rb
@@ -0,0 +1,16 @@
+require './lib/beat_box'
+require './lib/linked_list'
+require './lib/node'
+
+RSpec.describe BeatBox do
+  before :each do
+    @bb = BeatBox.new
+  end
+
+  describe '#initialize' do
+  it 'has a linked list that is empty' do
+
+  end
+
+
+end
