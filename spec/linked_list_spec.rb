6e95399073a6c0c99c9c4cd8bf4b4dd1e024bebe
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-26 17:54:14 -0700

    Add testing for ability to append multiple nodes

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -17,13 +17,12 @@ RSpec.describe LinkedList do
     expect(@list.head).to eq(nil)
   end
 
+
   it 'has data value for 1st node' do
     @list.append("doop")
     @list
     expect(@list).to be_instance_of(LinkedList)
   end
-
-  
 
   it 'counts the nodes' do
     @list.append("doop")
@@ -35,6 +34,66 @@ RSpec.describe LinkedList do
     @list.append("doop")
     expect(@list.to_string).to eq("doop")
   end
+end
 
+  describe 'multiple append function' do
+  
+  it 'has a list' do
+    list = LinkedList.new
+   expect(@list).to be_instance_of(LinkedList)
+  end
+
+  it 'has head equal to nil' do
+    expect(@list.head).to eq(nil)
+  end
+
+  it 'has a head equal to "doop"' do
+    @list.append("doop")
+    expect(@list.head.data).to eq("doop")
+  end
+
+  it 'has a list with the node "doop"' do
+    @list.append("doop")
+    expect(@list).to be_instance_of(LinkedList)
+  end
+
+  it 'has a head with value "doop"' do
+    @list.append("doop")
+    @list.head
+    expect(@list.head).to be_instance_of(Node)
+  end
+
+  it 'has a head that points to nil' do
+    @list.append("doop")
+    expect(@list.head.next_node).to eq(nil)
+  end
+
+  it 'has appended a node "doop"' do
+    @list.append("doop")
+    expect(@list.head.data).to eq("doop")
+  end
+
+    it 'appended the node "deep"' do
+      @list.append("doop")
+      @list.append("deep")
+      expect(@list).to be_instance_of(LinkedList)
+    end
+
+    it 'has a list that includes "deep" & "doop"' do
+      @list.append("doop")
+      @list.append("deep")
+      expect(@list).to be_instance_of(LinkedList)
+    end
+
+    # it 'has next node of "deep"' do
+    #   @list.append("doop")
+    #   @list.append("deep")
+      
+    #   last = @list.last_node
+    #   expect(@list.head.next_node).to eq(last.data)
+    # end
+
+    describe 'Iteration 2'
 end
 end
+
